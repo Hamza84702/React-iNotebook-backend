756db63053cb667820ad41bd89b4f2cfc99b25f4
Hamza Saleem <saleemh075@gmail.com>
2024-05-08 01:37:20 +0500

    resolve cors error

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const connectToMonogo = require('./db');
 
+
 connectToMonogo();
 
 const express = require('express')
+const cors = require("cors");
+
 const app = express()
 const port = 5000
 
-app.use(express.json())
+app.use(cors());
 
+app.use(express.json())
+ 
 //Available Routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
